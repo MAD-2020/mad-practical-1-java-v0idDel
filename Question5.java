2db1084a5ac1d6729129c98529f9cb4b4acdc15d
v0idDel <64129820+v0idDel@users.noreply.github.com>
2020-04-22 22:19:47 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Question5
 {
   public static void main(String[] args)
@@ -27,6 +27,36 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter the number of integers you will be entering: ");
+    int num = in.nextInt();
+
+    ArrayList<Integer> data = new ArrayList<>();
+    for (int i = 0; i < num; i++){
+      System.out.print("Enter the integer: ");
+      int input = in.nextInt();
+      data.add(input);
+    }
+    System.out.println(data);
+    in.close();
+
+    int mode = data.get(0);
+    int maxCount = 0;
+    
+    for (int i = 0; i < data.size(); i++){
+      int value = data.get(i);
+      int count = 1;
+      for (int j = i+1; j < data.size(); j++){
+        if (data.get(j) == value){
+          count++;
+        }
+        if (count > maxCount){
+          mode = value;
+          maxCount = count;
+        }
+      }
+    }
+    System.out.println(mode);
     
   }
 }
