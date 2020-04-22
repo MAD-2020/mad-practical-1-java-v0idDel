6cc8466e9c6faf9748b6ac99bafbc89f32731483
v0idDel <64129820+v0idDel@users.noreply.github.com>
2020-04-22 20:47:32 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -16,8 +16,19 @@ public class Question4
      * 
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
+
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter an integer: ");
+    int num = in.nextInt();
+    in.close();
+    
+    for (int i = num; i > 0; i--){
+      for (int j = 0; j < i ; j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
     
   }
 }
